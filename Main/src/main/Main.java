950985104ca358b5f0ec6af601290ee94aa471a6
FelipeReis1 <feliperg15@yahoo.com.br>
2021-01-20 17:29:18 -0300

    teste

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -15,10 +15,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
         System.out.println("Hello World!!!");
         System.out.println("Hey World!!!");
+        System.out.println("Bye World!!!");
     }
     
 }
