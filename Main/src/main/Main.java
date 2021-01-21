07704b53d08a40ca94055aaa69c2df09c1729dfe
Otavio <otavioaufegues@gmail.com>
2021-01-21 00:39:30 -0300

    tirando codigo teste

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -40,9 +40,6 @@ public class Main {
             }
         }
 
-        System.out.println("Hello World!!!");
-        System.out.println("Hey World!!!");
-        System.out.println("Bye World!!!!");
     }
 
 }
